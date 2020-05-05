2cbf01224ed60afeec250f356c9a16a11899d199
Sean <sean@covertalert.ai>
2020-05-05 00:29:42 -0400

    Committing change in regular version of xcode.

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -36,11 +36,9 @@ struct ContentView: View {
     
     var body: some View {
         GridStack(rows: 4, columns: 4) { row, col in
-            
             Image(systemName: "\(row * 4 + col).circle")
             
             Text("R\(row) C\(col)")
-            
         }
     }
 }

005cf7aa7bf2a6e97fa2c7b7ccbb10c8adb27459
Sean <sean@covertalert.ai>
2020-05-04 21:34:48 -0400

    Shows how to break down a complex view into smaller more managable view.

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -8,23 +8,26 @@
 
 import SwiftUI
 
+struct CapsuleText: View {
+    var text: String
+    
+    var body: some View {
+        Text(text)
+            .font(.largeTitle)
+            .padding()
+            .background(Color.blue)
+            .clipShape(Capsule())
+    }
+}
+
 struct ContentView: View {
     
     var body: some View {
         VStack(spacing: 10) {
-            Text("First")
-                .font(.largeTitle)
-                .padding()
+            CapsuleText(text: "First")
                 .foregroundColor(.white)
-                .background(Color.blue)
-                .clipShape(Capsule())
-            
-            Text("Second")
-                .font(.largeTitle)
-                .padding()
-                .foregroundColor(.white)
-                .background(Color.blue)
-                .clipShape(Capsule())
+            CapsuleText(text: "Second")
+                .foregroundColor(.yellow)
         }
         
     }

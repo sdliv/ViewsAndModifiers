17b1b5370bdc83dc84c09cf591a00a8d0542a307
Sean <sean@covertalert.ai>
2020-05-05 01:24:54 -0400

    Creates custom modifier that sets large text colored blue.

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -8,41 +8,28 @@
 
 import SwiftUI
 
-struct GridStack<Content: View> : View {
-    let rows: Int
-    let columns: Int
-    let content: (Int, Int) -> Content
-    
-    var body: some View {
-        VStack {
-            ForEach(0 ..< rows) { row in
-                HStack {
-                    ForEach(0 ..< self.columns) { column in
-                        self.content(row, column)
-                    }
-                }
-            }
-        }
-    }
-    
-    init(rows: Int, columns: Int, @ViewBuilder content: @escaping (Int, Int) -> Content) {
-        self.rows = rows
-        self.columns = columns
-        self.content = content
-    }
-}
 
 struct ContentView: View {
     
     var body: some View {
-        GridStack(rows: 4, columns: 4) { row, col in
-            Image(systemName: "\(row * 4 + col).circle")
-            
-            Text("R\(row) C\(col)")
-        }
+        Text("Hello World")
+        .titleStyle()
     }
 }
 
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.blue)
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        self.modifier(Title())
+    }
+}
 
 
 struct ContentView_Previews: PreviewProvider {

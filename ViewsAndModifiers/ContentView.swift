cd123842e237dc0eec07d57beaeb5988d75a77dc
Sean <sean@covertalert.ai>
2020-05-04 21:46:27 -0400

    Beginning: Create custom containers in SwiftUI

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -8,51 +8,10 @@
 
 import SwiftUI
 
-struct Title: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .font(.largeTitle)
-            .foregroundColor(.white)
-            .padding()
-            .background(Color.blue)
-            .clipShape(RoundedRectangle(cornerRadius: 10))
-    }
-}
-
-extension View {
-    func titleStyle () -> some View {
-        self.modifier(Title())
-    }
-}
-
-struct Watermark: ViewModifier {
-    var text: String
-    
-    func body(content: Content) -> some View {
-        ZStack(alignment: .bottomTrailing) {
-            content
-            
-            Text(text)
-                .font(.caption)
-                .foregroundColor(.white)
-                .padding(5)
-                .background(Color.black)
-        }
-    }
-}
-
-extension View {
-    func watermarked(with text: String) -> some View {
-        self.modifier(Watermark(text: text))
-    }
-}
-
 struct ContentView: View {
     
     var body: some View {
-        Color.blue
-            .frame(width: 300, height: 300)
-        .watermarked(with: "Hacking with Swift")
+        Text("Hello World")
     }
 }
 

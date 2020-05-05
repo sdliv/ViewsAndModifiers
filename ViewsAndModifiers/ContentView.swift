a7f47d9342edab2b36aad0692733eae5f118964a
Sean <sean@covertalert.ai>
2020-05-05 00:25:32 -0400

    Creates custom containers in SwiftUI: A GridStack

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -8,12 +8,44 @@
 
 import SwiftUI
 
+struct GridStack<Content: View> : View {
+    let rows: Int
+    let columns: Int
+    let content: (Int, Int) -> Content
+    
+    var body: some View {
+        VStack {
+            ForEach(0 ..< rows) { row in
+                HStack {
+                    ForEach(0 ..< self.columns) { column in
+                        self.content(row, column)
+                    }
+                }
+            }
+        }
+    }
+    
+    init(rows: Int, columns: Int, @ViewBuilder content: @escaping (Int, Int) -> Content) {
+        self.rows = rows
+        self.columns = columns
+        self.content = content
+    }
+}
+
 struct ContentView: View {
     
     var body: some View {
-        Text("Hello World")
+        GridStack(rows: 4, columns: 4) { row, col in
+            
+            Image(systemName: "\(row * 4 + col).circle")
+            
+            Text("R\(row) C\(col)")
+            
+        }
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {

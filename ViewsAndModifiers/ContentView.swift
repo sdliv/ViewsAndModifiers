0e55c42b139413b8eb27b14e40507fc7f5c4291d
Sean <sean@covertalert.ai>
2020-05-04 21:45:35 -0400

    Remember, modifiers create and return a new view, not modify an existing view

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -25,11 +25,34 @@ extension View {
     }
 }
 
+struct Watermark: ViewModifier {
+    var text: String
+    
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            
+            Text(text)
+                .font(.caption)
+                .foregroundColor(.white)
+                .padding(5)
+                .background(Color.black)
+        }
+    }
+}
+
+extension View {
+    func watermarked(with text: String) -> some View {
+        self.modifier(Watermark(text: text))
+    }
+}
+
 struct ContentView: View {
     
     var body: some View {
-        Text("Hello World")
-        .titleStyle()
+        Color.blue
+            .frame(width: 300, height: 300)
+        .watermarked(with: "Hacking with Swift")
     }
 }
 
